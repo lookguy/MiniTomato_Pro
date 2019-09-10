143a31d9b49133eaf97a6b6a954c2421574bb1dc
想飞天的招雄彬 <614619114@qq.com>
2019-09-10 19:38:22 +0800

    个人页面

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -6,19 +6,13 @@ Page({
    */
   clearTimeId: null,
   data: {
-    count: 5,
+    count: 1500,
     time: null,
     isReady: true,
     visible: false,
     isAgain: false,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-
-  },
   //  根据秒数格式化时间
   formatTime() {
     this.data.isReady = true
@@ -65,8 +59,9 @@ Page({
   },
   //确定放弃
   send(e) {
-    console.log(e.detail)
-    this.close()
+    wx.navigateBack({
+      delta:1
+    })
   },
   //关闭
   close() {
@@ -89,46 +84,4 @@ Page({
   onReady: function() {
     this.start()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
