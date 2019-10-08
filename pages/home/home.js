1a69029c8dd1af83220c73509ec5e12257553f7d
想飞天的招雄彬 <614619114@qq.com>
2019-10-09 02:13:34 +0800

    增加离线的基本使用

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,57 +12,84 @@ Page({
     showComfirm: false,
     target_value: "",
     target_update: {},
-    planning: []
+    planning: [{
+      description: "你可以通过点击下方的‘创建计划’来创建一个记录，通过点击记录里的圆圈来完成计划，但别忘了先点击闹钟开始行动"
+    }]
   },
   onShow() {
     this.initData()
   },
   /* 自定义事件 */
   initData() {
-    http.get("/todos?completed=false")
-      .then(res => {
-        if (res.statusCode == 200) {
-          let resources = res.data.resources;
-          this.data.planning = resources;
-          this.setData({
-            planning: resources
-          });
-        }
-      })
-      .catch(err => {
-        throw (err)
-      });
+    if (wx.getStorageSync('userInfo') || wx.getStorageSync('X-token')) {
+      http.get("/todos?completed=false")
+        .then(res => {
+          if (res.statusCode == 200) {
+            let resources = res.data.resources;
+            this.data.planning = resources;
+            this.setData({
+              planning: resources
+            });
+          }
+        })
+        .catch(err => {
+          throw (err)
+        });
+    }
   },
   addTodos(opt) {
     // console.log(opt.detail);
     let id = this.data.planning.length
     this.data.showComfirm = false;
-    http.post("/todos", {
-        description: opt.detail
-      })
-      .then(res => console.log(res))
-      .catch(err => {
-        throw (err)
-      });
-    this.setData({
-      'showComfirm': this.data.showComfirm
-    })
-    this.initData();
-  },
-  completion(e){
-    let recordID = e.currentTarget.dataset.target.id
-    http.put(`/todos/${recordID}`, { completed: true })
-      .then(res => {
-        if (res.statusCode == 200) {
-          this.initData();
+    if (wx.getStorageSync('userInfo') || wx.getStorageSync('X-token')) {
+      http.post("/todos", {
+          description: opt.detail
+        })
+        .then(res => {
           wx.showToast({
-            title: '本条记录已完成',
+            title: '创建完毕',
           })
-        };
+          this.initData()
+        })
+        .catch(err => {
+          throw (err)
+        });
+      this.setData({
+        'showComfirm': this.data.showComfirm
       })
-      .catch(err => {
-        throw (err);
-      });
+      this.initData();
+    } else {
+      this.data.planning.push({
+        description: opt.detail,
+        id
+      })
+      this.setData({ planning: this.data.planning, showComfirm:false})
+    }
+  },
+  completion(e) {
+    let recordID = e.currentTarget.dataset.target.id
+    if (wx.getStorageSync('userInfo') || wx.getStorageSync('X-token')) {
+      http.put(`/todos/${recordID}`, {
+        completed: true
+      })
+        .then(res => {
+          if (res.statusCode == 200) {
+            this.initData();
+            wx.showToast({
+              title: '本条记录已完成',
+            })
+          };
+        })
+        .catch(err => {
+          throw (err);
+        });
+    }else{
+      let del_index = this.data.planning.findIndex(item=>{
+        return item.id == recordID
+      })
+      this.data.planning.splice(del_index,1)
+      this.setData({ planning: this.data.planning})
+    }
   },
   update(e) {
     this.data.showComfirm_update = true
@@ -73,17 +100,27 @@ Page({
     })
   },
   updateTodos(e) {
-    http.put(`/todos/${this.data.target_update.id}`, {description:e.detail})
-    .then(res=>{
-      console.log(res)
-      if(res.statusCode==200){
-        this.initData();
-      };
-      this.setData({"showComfirm_update":false});
-    })
-    .catch(err=>{
-      throw(err)
-    })
+    if (wx.getStorageSync('userInfo') || wx.getStorageSync('X-token')) {
+      http.put(`/todos/${this.data.target_update.id}`, {
+        description: e.detail
+      })
+        .then(res => {
+          console.log(res)
+          if (res.statusCode == 200) {
+            this.initData();
+          };
+          this.setData({
+            "showComfirm_update": false
+          });
+        })
+        .catch(err => {
+          throw (err)
+        })
+    }else{
+      wx.showToast({
+        title: '需要你登陆,也可以再创建',icon: 'none',
+      })
+    }
   },
   tap_button() {
     this.data.showComfirm = true
--- pages/mycenter/mycenter.js
+++ pages/mycenter/mycenter.js
@@ -1,34 +1,91 @@
 // pages/mycenter/mycenter.js
 const {
   http
-} = require("../../lib/http.js")
+} = require('../../lib/http.js');
+const {
+  app_id,
+  app_secret
+} = getApp().globalData;
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     tapWord: "history",
-    tomatoes: null,
-    todos: null,
+    dataStatus: 0,
+    tomatoes: {},
+    todos: {},
+    swiper_current: 0,
   },
   changeTab: function(e) {
     this.data.tapWord = e.currentTarget.dataset.tap;
     if (e.currentTarget.dataset.tap=="history"){
       this.getTomatoes()
+      this.data.swiper_current = 0
     }else{
       this.getTodos()
+      this.data.swiper_current = 1
     }
     this.setData({
-      tapWord: e.currentTarget.dataset.tap
+      tapWord: e.currentTarget.dataset.tap,
+      swiper_current: this.data.swiper_current
+    })
+  },
+  choseTab(e){
+    switch(e.detail.current){
+      case 0:
+        this.data.tapWord = "history";
+        break;
+      case 1:
+        this.data.tapWord = "compelete";
+        break;
+      default:
+        return;
+    }
+    this.setData({ "tapWord": this.data.tapWord})
+  },
+
+  login(e) {
+    const loginUrl = '/sign_in/mini_program_user'
+    const homeUrl = '/pages/home/home'
+    let encrypted_data = e.detail.encryptedData
+    let iv = e.detail.iv
+    let code
+    wx.login({
+      success(res) {
+        code = res.code
+        http.post(loginUrl, {
+          code,
+          iv,
+          encrypted_data,
+          app_id,
+          app_secret
+        })
+          .then(res => {
+            wx.setStorageSync('userInfo', res.data.resource)
+            wx.setStorageSync('X-token', res.header['X-token'])
+            wx.reLaunch({
+              url: homeUrl,
+            })
+          })
+          .catch(err => console.log(err))
+      },
+      fail(err) {
+        throw (err, 2)
+      }
     })
   },
 
   onShow() {
     this.getTomatoes();
     this.getTodos();
+    if (wx.getStorageSync('userInfo') || wx.getStorageSync('X-token')){
+      this.data.dataStatus = 1
+      this.setData({ dataStatus: 1})
+    }
   },
   getTomatoes() {
-    http.get("/tomatoes", {
+    return http.get("/tomatoes", {
         is_group: "yes"
       })
       .then(res => {
@@ -42,13 +99,13 @@ Page({
       })
   },
   getTodos() {
-    http.get("/todos", {
+    return http.get("/todos", {
         is_group: "yes"
       })
       .then(res => {
         if (res.statusCode == 200) {
           this.data.todos = res.data.resources
-          this.setData({ "todos": res.data.resources})
+          this.setData({ "todos": res.data.resources })
         }
       })
       .catch(err => {
